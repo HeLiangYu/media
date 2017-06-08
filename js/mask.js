06ea8f9026bb6f04ed0e13767dad862881acd93f
HeLiangYu <928296545@qq.com>
2017-06-08 18:29:16 +0800

    在线视频点播平台
    一个自制的在线视频点播品台

---
+++ js/mask.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+	$(".box1").hover(function(){
+					$(".mask1").	addClass("mask");
+				}, function(){
+					$(".mask1").removeClass("mask");
+				});
+				
+				$(".box2").hover(function(){
+					$(".mask2").	addClass("mask");
+				}, function(){
+					$(".mask2").removeClass("mask");
+				});
+				
+				$(".box3").hover(function(){
+					$(".mask3").	addClass("mask");
+				}, function(){
+					$(".mask3").removeClass("mask");
+				});
+				
+				$(".box4").hover(function(){
+					$(".mask4").	addClass("mask");
+				}, function(){
+					$(".mask4").removeClass("mask");
+				});
+				
+				$(".box5").hover(function(){
+					$(".mask5").	addClass("mask");
+				}, function(){
+					$(".mask5").removeClass("mask");
+				});
+				
+				$(".box6").hover(function(){
+					$(".mask6").	addClass("mask");
+				}, function(){
+					$(".mask6").removeClass("mask");
+				});
+				
+				$(".box7").hover(function(){
+					$(".mask7").	addClass("mask");
+				}, function(){
+					$(".mask7").removeClass("mask");
+				});
+				
+				$(".box8").hover(function(){
+					$(".mask8").	addClass("mask");
+				}, function(){
+					$(".mask8").removeClass("mask");
+				});
+				
+				$(".box9").hover(function(){
+					$(".mask9").	addClass("mask");
+				}, function(){
+					$(".mask9").removeClass("mask");
+				});
+});
